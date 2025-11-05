d10077aed1370e0abb632bdec50101a59c40e0cd
unknown <Gastnutzer@IWW216.iww.privat>
2025-11-05 17:37:15 +0100

    initial thoughts on mandatory steps

---
+++ R/Connect Tree Inventory.R
@@ -0,0 +1,18 @@
+
+
+#load inventory
+
+#inventory old
+
+#inventory new
+
+#check for location and DBH --> MATCH
+
+#check for tree updates:
+#-old tree updated (grew/missing)
+#-new tree found (>7cm DBH)
+
+#validate updates:
+#-new trees found around 7cm DBH
+
+#save updated inventory
