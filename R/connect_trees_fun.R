99466d714315393acd89dcbc50572b28d6241172
Weidenfeller <Weidenfeller@IWW119.iww.privat>
2025-12-15 17:49:54 +0100

    cleaned Redundancies

--- R/connect_trees_fun.R
+++ R/connect_trees_fun.R
@@ -182,19 +182,3 @@ connect_trees <- function(
   )
 }
 
-
-
-
-# Beispielaufruf:
-result <- connect_trees(
-  inventur_alt_path = "path/to/Inventur_alt.csv",
-  inventur_neu_path = "path/to/Inventur_neu.csv"
-)
-
-transformed <- result$Inventur_neu_transformed
-head(transformed)
-
-# Optional: plotten
-plot(transformed$X, transformed$Y, pch = 19, col = "black",cex = 0.7)
-points(result$Inventur_alt_filtered$X, result$Inventur_alt_filtered$Y, pch = 20, col = "red", cex = 0.5)
- 
